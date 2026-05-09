d01ce30bc3b9be333aecbe1de216e6a126daa3ee
Diego Barbagallo <256335338+Dieeegooo@users.noreply.github.com>
2026-05-09 15:48:53 +0200

    template di base con laravel installato

---
+++ frontend/main.js
@@ -0,0 +1,217 @@
+const host = "http://localhost:3000";
+const getListButton = document.getElementById("get-list-button");
+const postListName = document.getElementById("post-list-name");
+const postListButton = document.getElementById("post-list-button");
+const resultList = document.getElementById("result-list");
+const description = document.getElementById("descrizione");
+const putListName = document.getElementById("put-list-name");
+let putListId = document.getElementById("put-list-id");
+const putListButton = document.getElementById("put-list");
+const descriptionListName = document.getElementById("description-list")
+const descriptionListButton = document.getElementById("put-list-description")
+let postNoteId = document.getElementById("post-note-id");
+let deleteListId = document.getElementById("del-list-id");
+let descriptionListId = ""
+let id = null
+
+async function reloadListTable() {
+  apiRequest(host + "/list", "GET", {})
+    .then((data) => {
+      console.log(data); // TODO: delete this line
+      resultList.innerHTML = "";
+      const table = document.createElement("table");
+
+      const th = document.createElement("th");
+      th.textContent = "List";
+      th.colSpan = "5";
+      const tr = document.createElement("tr");
+      tr.appendChild(th);
+      table.appendChild(tr);
+
+      for (const list of data) {
+        const tr = document.createElement("tr");
+        const td1 = document.createElement("td");
+        td1.innerHTML = list.id;
+        const td2 = document.createElement("td");
+        td2.innerHTML = list.title;
+        const td3 = document.createElement("td");
+        td3.style.cursor = "pointer";
+        td2.style.cursor = "pointer"
+        td3.innerHTML = list.descrizione;
+        const td4 = document.createElement("td");
+        td4.innerHTML = "&#9003";
+        td2.addEventListener("click", () => {
+          putListId = list.id;
+          deleteListId = list.id;
+          postNoteId = list.id;
+        })
+        td3.addEventListener("click", () => {
+          descriptionListId = list.id;
+          descriptionListName.focus();
+        })
+        td4.style.cursor = "pointer"
+        td4.addEventListener("click", async () => {
+          await apiRequest(host + "/list/" + list.id, "DELETE", {});
+          await reloadListTable();
+        })
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+        tr.appendChild(td4);
+        table.appendChild(tr);
+      }
+      resultList.appendChild(table);
+    })
+    .catch((error) => console.error(error));
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  reloadListTable();
+  reloadListTableNote();
+});
+
+postListButton.addEventListener("click", () => {
+  if (postListName.value == "") {
+    return console.log("nome o descrizione non validi");
+  }
+
+  apiRequest(host + "/list", "POST", { name: postListName.value, descrizione: description.value });
+
+  postListName.value = "";
+  description.value = "";
+
+  reloadListTable();
+});
+
+putListButton.addEventListener("click", () => {
+  if (putListId.value == "" || putListName.value == "") {
+    return console.log("id o nome non validi");
+  }
+
+  apiRequest(host + "/list/" + putListId, "PUT", { name: putListName.value });
+
+  putListId.value = "";
+  putListName.value = "";
+
+  reloadListTable();
+});
+
+
+descriptionListButton.addEventListener("click",async () => {
+  if (descriptionListName.value == "") {
+    return console.log("id o nome non validi");
+  }
+  await apiRequest(host + "/desc/" + descriptionListId, "PUT", { desc: descriptionListName.value });
+
+  descriptionListName.value = "";
+
+  await reloadListTable();
+})
+
+
+
+const getListButtonNote = document.getElementById("get-list-button-note");
+const postListNameNote = document.getElementById("post-list-name-note");
+const postListButtonNote = document.getElementById("post-list-button-note");
+const resultListNote = document.getElementById("result-list-note");
+const putListNameNote = document.getElementById("put-list-name-note");
+let putListIdNote = ""
+const putListButtonNote = document.getElementById("put-list-note");
+
+async function reloadListTableNote() {
+  apiRequest(host + "/note", "GET", {})
+    .then((data) => {
+      resultListNote.innerHTML = "";
+      const table = document.createElement("table");
+
+      const th = document.createElement("th");
+      th.textContent = "Note";
+      th.colSpan = "5";
+      const tr = document.createElement("tr");
+      tr.appendChild(th);
+      table.appendChild(tr);
+
+      for (const note of data) {
+        const tr = document.createElement("tr");
+        const td1 = document.createElement("td");
+        td1.innerHTML = note.id;
+        const td2 = document.createElement("td");
+        td2.innerHTML = note.name;
+        td2.style.cursor = "pointer";
+          td2.addEventListener("click", () => {
+            putListIdNote = note.id;
+            putListNameNote.focus();
+        })
+        const td3 = document.createElement("td");
+        td3.innerHTML = note.list_id;
+        const td4 = document.createElement("td");
+        td4.innerHTML = note.state;
+        const td5 = document.createElement("td");
+        td5.innerHTML = "&#9003";
+        td5.style.cursor = "pointer";
+        td5.addEventListener("click", async () => {
+
+          await apiRequest(host + "/note/" + note.id, "DELETE", {});
+
+          await reloadListTableNote();
+        });
+
+        td4.addEventListener("click", () => {
+          if (note.state == "todo") {
+            // cambia in done
+            apiRequest(host + "/note/" + note.id + "/todo", "PUT", { state: "done" }).then(() => {
+              setTimeout(() => {
+                reloadListTableNote();
+              }, 20);
+            });
+          } else {
+            // cambia in todo
+            apiRequest(host + "/note/" + note.id + "/todo", "PUT", { state: "todo" }).then(() => {
+              setTimeout(() => {
+                reloadListTableNote();
+              }, 20);
+            });
+          }
+        });
+
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+        tr.appendChild(td4);
+        tr.appendChild(td5);
+        table.appendChild(tr);
+      }
+      resultListNote.appendChild(table);
+    })
+    .catch((error) => console.error(error));
+}
+
+reloadListTableNote();
+
+postListButtonNote.addEventListener("click", () => {
+  if (postListNameNote.value == "" ) {
+    return console.log("id o nome non validi");
+  }
+
+  //chiedere a derteo se gli passi che todo
+
+  apiRequest(host + "/note", "POST", { name: postListNameNote.value, state: "todo", list_id: postNoteId });
+
+  postListNameNote.value = "";
+
+  reloadListTableNote()
+});
+
+putListButtonNote.addEventListener("click", () => {
+  if (putListNameNote.value == "") {
+    return console.log("id o nome non validi");
+  }
+
+  apiRequest(host + "/note/" + putListIdNote, "PUT", { name: putListNameNote.value });
+
+  putListNameNote.value = "";
+
+  reloadListTableNote();
+});
+
+// ripassare quello che ho scritto su perplexity per eliminare un intera riga con la DELETE grazie a un bottone serve l'id scorrendo un for
